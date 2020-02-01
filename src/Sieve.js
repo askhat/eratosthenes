18cd3ef807ef80834369271c8b6d7ce55ab13fb9
Askhat Bikmetov <lolborgini@gmail.com>
2020-02-01 21:41:27 +0300

    Update Sieve component

--- src/Sieve.js
+++ src/Sieve.js
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { eratosthenes } from "./Eratosthenes";
+import { eratosthenes as E } from "./eratosthenes/Cargo.toml";
 import { Knobs } from "./Knobs";
 
 export function Sieve() {
   let [grid, setGrid] = useState([]);
-  let [rowSize, setRowSize] = useState(6);
-  let [sieveSize, setSieveSize] = useState(100);
+  let [rowSize, setRowSize] = useState(31);
+  let [sieveSize, setSieveSize] = useState(1001);
 
   useEffect(() => {
-    let numbers = eratosthenes(sieveSize).map((prime, value) => ({
-      prime,
-      value
-    }));
+    let numbers = E(sieveSize).map((prime, value) => ({ prime, value }));
     let rows = [];
-    for (let i = 2; i <= numbers.length; i += rowSize) {
+    for (let i = 0; i <= numbers.length; i += rowSize) {
       rows.push(numbers.slice(i, i + rowSize));
     }
     setGrid(rows);
@@ -29,11 +26,11 @@ export function Sieve() {
       />
       <table>
         <tbody>
-          {grid.map((row, i) => (
-            <tr key={i}>
+          {grid.map(row => (
+            <tr key={row.toString()}>
               {row.map(({ prime, value }) => (
-                <td key={value} style={{ background: prime ? "red" : "white" }}>
-                  {value}
+                <td key={value} style={{ background: prime && "black" }}>
+                  <span style={{ color: prime && "white" }}>{value}</span>
                 </td>
               ))}
             </tr>
